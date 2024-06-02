2c59b821c932d140aad7021acf6499def5da9a97
Nathan Luiz <nathanluiz33@gmail.com>
2024-06-02 11:54:33 -0300

    removing comments

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -45,38 +45,6 @@ graph::Instance read_graph(std::istream& is)
       }
     }
   }
-  // for (auto ei = boost::edges(GG); ei.first != ei.second; ++ei.first) {
-  //   graph::Edge e = *ei.first;
-  //   std::cout << e << "\n";
-  //   // std::cout << " " << boost::source(e, GG) << " " << boost::target(e, GG) << " " << GG[e].color << std::endl;
-  //   // auto x = boost::source(e, GG), y = boost::target(e, GG), color = GG[e].color;
-  //   // graph::Edge a;
-  //   // std::tie(a, std::ignore) = boost::add_edge(x, y, G[color]);
-  //   // G[color][a].id = GG[e].id;
-  //   // G[color][a].color = color;
-  // }
-  // for (const auto &edge : boost::make_iterator_range(boost::edges(GG))) {
-  //   // std::cout << edge << "\n";
-  //   auto x = boost::source(edge, GG), y = boost::target(edge, GG), color = GG[edge].color;
-  //   graph::Edge a;
-  //   std::tie(a, std::ignore) = boost::add_edge(x, y, G[color]);
-  //   G[color][a].id = GG[edge].id;
-  //   G[color][a].color = color;
-  // }
-  // for (const auto& edge : boost::edges(GG)) {
-
-    // int x = boost::source(edge, GG), y = boost::target(edge, GG), color = GG[edge].color;
-    // // std::tie(e, b) = boost::edge(x, y, GG);
-    // graph::Edge e; bool b;
-    // std::tie(e, b) = boost::edge(y, x, GG);
-    // for (int id = 0; id < boost::num_edges(GG); id++) {
-    //   // if (boost::ed)
-    // }
-    // std::cout << " " << x << " " << y << " " << color << " " << b << " " << e << std::endl;
-    // if (not b) {
-    //   return false;
-    // }
-  // }
 
   return {std::make_shared<graph::Graph>(GG), G};
 }
@@ -87,42 +55,7 @@ int main(int argc, char** argv)
 
   size_t n = num_vertices(*GG);
 
-  // for (int i = 0; i < n; i++) {
-  //   for (const auto& vertex : boost::make_iterator_range(boost::vertices(G[i]))) {
-  //     std::cout << "Vertex " << vertex << " has degree " << boost::degree(vertex, G[i]) << std::endl;
-  //     for (const auto& edge : boost::make_iterator_range(boost::out_edges(vertex, G[i]))) {
-  //       std::cout << "Edge " << G[i][edge].id << " has color " << G[i][edge].color << std::endl;
-  //     }
-  //   }
-  // }
-  // for (const auto& vertex : boost::make_iterator_range(boost::vertices(*GG))) {
-  //   std::cout << "Vertex " << vertex << " has degree " << boost::degree(vertex, *GG) << std::endl;
-  //   for (const auto& edge : boost::make_iterator_range(boost::out_edges(vertex, *GG))) {
-  //     std::cout << "Edge " << (*GG)[edge].id << " has color " << (*GG)[edge].color << std::endl;
-  //   }
-  // }
-  
   std::vector<bool> usedColors (n), usedVertex (n), usedEdges (n * (n - 1) / 2);
-
-  // We can greedily increase the path until it has size greather than n / 2
-  // due to degree constraints: deg(v) >= n / 2 for all v
-  // Path path;
-
-  // while (path.size() <= n / 2) {
-  //   auto u = path.back();
-  //   int c = std::find(usedColors.begin(), usedColors.end(), false) - usedColors.begin();
-  //   for (const auto &v : boost::make_iterator_range(boost::adjacent_vertices(u, G[c]))) {
-  //       if(not usedVertex[v]) {
-  //           usedColors[c] = true;
-  //           usedVertex[v] = true;
-  //           path.push_back(v);
-  //           break;
-  //       }
-  //   }
-  //   if(not usedColors[c]) {
-  //       throw std::runtime_error("?");
-  //   }
-  // }
 
   return EXIT_SUCCESS;
 }
