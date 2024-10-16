8f556caf7c6552362f81dc07613145c2d57da02f
Nathan Luiz <nathanluiz33@gmail.com>
2024-10-15 21:48:12 -0300

    fixing some bugs on python code

--- python/src/increment.py
+++ python/src/increment.py
@@ -40,8 +40,6 @@ def handle_path(G: Graph, path: Path):
     else:
         cx = path.edges[-1].color
         cy = next(i for i in range(n) if not colors_in_path[i])
-
-
         path.pop_back()
         x, y = path.vertices[0], path.vertices[-1]
         print("cx, cy, x, y", cx, cy, x, y)
@@ -72,8 +70,11 @@ def handle_path(G: Graph, path: Path):
             bY, edgeY = G.check_edge(u, y, cy)
 
             if bX and bY:
-                vertices = path.vertices[:i + 1] + [y] + path.vertices[i + 1:]
-                edges = path.edges[:i] + [edgeY] + path.edges[i:]
+                vertices = path.vertices[:i + 1] + [y]
+                edges = path.edges[:i] + [edgeY]
+                for j in range(len(path.vertices) - 1, i + 1, -1):
+                    vertices.append(path.vertices[j - 1])
+                    edges.append(path.edges[j - 1])
                 edges.append(edgeX)
                 return Cycle(G, vertices, edges)
 
@@ -128,10 +129,10 @@ def handle_cycle(G: Graph, cycle: Cycle):
             u, v = cycle.vertices[i], cycle.vertices[(i + 1) % cycle_sz]
             color = pos_color_cic[i]
 
+            assert new_color_id[color] == i
             for tgt in G.adjacency[color][u]:
                 if tgt == v:
                     continue
-                assert new_color_id[color] == i
 
                 in_degree[tgt] += 1
                 incoming_neighborhood[tgt].append(u)
@@ -155,8 +156,8 @@ def handle_cycle(G: Graph, cycle: Cycle):
             edges.pop()
 
             _, edge = G.check_edge(vertices[-1], y, pos_color_cic[i])
-            _, edge2 = G.check_edge(y, vertices[0], cy)
-
+            __, edge2 = G.check_edge(y, vertices[0], cy)
+            assert(_ and __)
             vertices.append(y)
             edges.append(edge)
             edges.append(edge2)
@@ -165,9 +166,107 @@ def handle_cycle(G: Graph, cycle: Cycle):
 
         for i in I:
             if i in _I:
-                return find_answer(cycle.vertices.copy(), cycle.edges.copy(), i)
+                return find_answer(cycle.vertices, cycle.edges, i)
+        
+        vertices = cycle.vertices.copy()
+        edges = cycle.edges.copy()
 
-        raise RuntimeError("Should not reach here. Did not find answer.")
+        found_vertex = False
+        for i in range(cycle_sz):
+            u = vertices[i]
+            if in_degree[u] > n // 2 - 1:
+                found_vertex = True
+                vertices = vertices[i:] + vertices[:i]  # Rotaciona os vértices
+                edges = edges[i:] + edges[:i]  # Rotaciona as arestas
+
+                # Reconstrói o que precisa
+                for i in range(cycle_sz):
+                    color = edges[i].color
+                    new_color_id[color] = i
+                    pos_cic[vertices[i]] = i
+                    pos_color_cic[i] = edges[i].color
+                break
+
+        if found_vertex:
+            I1 = find_adjacency(y, pos_color_cic[0], pos_cic)
+            In = find_adjacency(y, cy, pos_cic)
+            In = [(u - 1 + cycle_sz) % cycle_sz for u in In]
+
+            assert len(I1) + len(In) >= n
+
+            J = -1
+            removed_color = -1
+            new_vertices = []
+            new_edges = []
+            new_pos = [0] * n
+
+            print("I1", I1)
+            print("In", In)
+
+            for i in I1:
+                if i in In:
+                    if i == 0:
+                        return find_answer(vertices, edges, 0)
+
+                    J = i
+                    removed_color = pos_color_cic[J]
+                    print("J", J)
+                    print("vertices", vertices)
+                    # Constrói o novo caminho
+                    new_vertices = vertices[1:i+1] + [y] + vertices[i+1:cycle_sz] + [vertices[0]]
+                    print("new_vertices", new_vertices)
+
+                    for j in range(len(new_vertices)):
+                        new_pos[new_vertices[j]] = j
+
+                    new_edges = edges[1:i] + [
+                        G.check_edge(vertices[i], y, pos_color_cic[i])[1],
+                        G.check_edge(y, vertices[0], cy)[1]
+                    ] + edges[i+1:cycle_sz]
+
+                    assert G.check_edge(vertices[i], y, pos_color_cic[i])[0] and G.check_edge(y, vertices[0], cy)[0]
+
+                    print("new_vertices", new_vertices)
+                    for edge in new_edges:
+                        pprint(vars(edge))
+                    assert len(new_vertices) == n and len(new_vertices) == len(new_edges) + 1
+                    break
+
+            # Caso em que fecha o ciclo bonitinho
+            found, edge = G.check_edge(new_vertices[0], new_vertices[-1], pos_color_cic[J])
+            if found:
+                new_edges.append(edge)
+                print("DEU RUIM AQUI", new_vertices)
+                for edge in new_edges:
+                    pprint(vars(edge))
+                return Cycle(cycle.G, new_vertices, new_edges)
+
+            # Vamos achar J1 e Jn
+            J1 = find_adjacency(new_vertices[0], pos_color_cic[J], new_pos)
+            J1 = [(u - 1 + n) % n for u in J1]
+
+            Jn = incoming_neighborhood[cycle.vertices[0]]
+            Jn = [new_pos[u] for u in Jn]
+
+            for i in J1:
+                if i in Jn:
+                    # Achamos a resposta
+                    if new_vertices[i + 1] == y:
+                        continue
+
+                    final_vertices = new_vertices[:i+1] + [new_vertices[-1]] + new_vertices[i+1:][::-1]
+
+                    assert len(final_vertices) == n
+
+                    final_edges = new_edges[:i] + [
+                        G.check_edge(new_vertices[i], new_vertices[-1], new_edges[i].color)[1]
+                    ] + new_edges[i+1:][::-1] + [
+                        G.check_edge(new_vertices[i+1], new_vertices[0], removed_color)[1]
+                    ]
+
+                    return Cycle(cycle.G, final_vertices, final_edges)
+
+        raise RuntimeError("Should not reach here. Did not find answer :(")
     else:
         for i in [miss1, miss2]:
             for u in range(n):
